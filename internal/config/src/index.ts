f227546ca1f0e797b95988b228ed752c33fb9aa9
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-06-05 13:16:47 -0500

    feat(config): integrates QA team to platform (#61)

---
+++ internal/config/src/index.ts
@@ -0,0 +1,12 @@
+export { reactTestingLibrary, vitestReact } from '@kurocado-studio/qa/web';
+export { faker, defineConfig } from '@kurocado-studio/qa';
+export {
+  prettierConfig,
+  semanticReleaseInternalConfig,
+  semanticReleaseAppConfig,
+  semanticReleaseNpmConfig,
+  eslintNodeConfig,
+  eslintReactConfig,
+  commitLintConfig,
+  defineESLintConfig,
+} from '@kurocado-studio/styleguide';
---
+++ teams/qa/packages/qa/src/index.ts
@@ -0,0 +1,4 @@
+export * from './common/vitestConfig';
+export { faker } from '@faker-js/faker';
+
+export { defineConfig, mergeConfig } from 'vitest/config';
