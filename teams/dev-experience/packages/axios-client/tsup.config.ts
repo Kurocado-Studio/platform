453e2a21e10fca0ba8eccd0dd9221a7d6546ceff
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-06-16 10:58:03 -0500

    fix(config): maps exports on package.json

--- teams/dev-experience/packages/axios-client/tsup.config.ts
+++ teams/dev-experience/packages/axios-client/tsup.config.ts
@@ -2,12 +2,15 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => ({
-  entry: ['./src/index.ts'],
+  entry: ['src/react/exports.ts', 'src/vue/exports.ts', 'src/node/exports.ts'],
   format: ['esm'],
   dts: true,
   clean: true,
   splitting: true,
   treeshake: true,
-  external: ['react'],
+  sourcemap: true,
+  minify: true,
+  target: 'esnext',
+  external: ['react', 'vue'],
   ...options,
 }));
