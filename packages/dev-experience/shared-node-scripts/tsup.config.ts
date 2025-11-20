dece363790597cd17d3fb021463cc1c6f811a03e
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-11-20 13:36:11 -0600

    fix(config): exports CJS module for node only envs

--- packages/dev-experience/shared-node-scripts/tsup.config.ts
+++ packages/dev-experience/shared-node-scripts/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => ({
   entry: ['src/index.ts'],
-  format: ['esm'],
+  format: ['esm', 'cjs'],
   dts: true,
   clean: true,
   splitting: true,
